a836c3c9d46d85025b633c966c700b4c6e713cff
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-18 14:32:42 -0400

    generate and route to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import {LandingPageComponent} from "./features/landing-page/landing-page.compone
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./features/landing-page/landing-page.component').then(mod => mod.LandingPageComponent) },
+  { path: 'home', loadComponent: () => import('./features/home-page/home-page.component').then(mod => mod.HomePageComponent) },
 ];
