abc2ef40a2b3ab74aadac8a37f94c0ee06ee4a2e
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-18 13:59:20 -0400

    create logo component

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {LogoComponent} from "../../shared/logo/logo.component";
 
 @Component({
   selector: 'ldnf-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    LogoComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
