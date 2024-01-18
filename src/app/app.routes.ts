aac549eb722ba2a59eb8aa8c733293df5358902a
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-18 13:44:40 -0400

    setup landing page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from "./features/landing-page/landing-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./features/landing-page/landing-page.component').then(mod => mod.LandingPageComponent) },
+];
