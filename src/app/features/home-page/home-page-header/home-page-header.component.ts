fb278c73a420dff8c44b5a9d01452ea5c67d1d29
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-18 14:36:59 -0400

    use home page header in home page

--- src/app/features/home-page/home-page-header/home-page-header.component.ts
+++ src/app/features/home-page/home-page-header/home-page-header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {LogoComponent} from "../../../shared/logo/logo.component";
 
 @Component({
   selector: 'ldnf-home-page-header',
   standalone: true,
-  imports: [],
+    imports: [
+        LogoComponent
+    ],
   templateUrl: './home-page-header.component.html',
   styleUrl: './home-page-header.component.css'
 })
--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {LogoComponent} from "../../shared/logo/logo.component";
+import {HomePageHeaderComponent} from "./home-page-header/home-page-header.component";
 
 @Component({
   selector: 'ldnf-home-page',
   standalone: true,
   imports: [
-    LogoComponent
+    LogoComponent,
+    HomePageHeaderComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
