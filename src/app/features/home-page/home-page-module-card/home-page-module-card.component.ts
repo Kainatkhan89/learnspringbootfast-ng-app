46202da0d07dbf3a8cf9362fff7dd68288637fb1
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-18 23:31:30 -0400

    create reusable module icons

--- src/app/features/home-page/home-page-module-card/home-page-module-card.component.ts
+++ src/app/features/home-page/home-page-module-card/home-page-module-card.component.ts
@@ -1,16 +1,38 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IModule} from "../../../core/models/learning-path/module.model";
 import {PillComponent} from "../../../shared/pill/pill.component";
+import {ModuleIconComponent} from "../../../shared/icons/module-icon/module-icon.component";
 
 @Component({
   selector: 'ldnf-home-page-module-card',
   standalone: true,
   imports: [
-    PillComponent
+    PillComponent,
+    ModuleIconComponent
   ],
   templateUrl: './home-page-module-card.component.html',
   styleUrl: './home-page-module-card.component.css'
 })
-export class HomePageModuleCardComponent {
+export class HomePageModuleCardComponent implements OnInit {
   @Input() moduleData: IModule | undefined;
+  totalDurationFormatted: string | undefined;
+
+  ngOnInit() {
+    this.calculateTotalDuration();
+  }
+
+  private calculateTotalDuration() {
+    if (this.moduleData && this.moduleData.tutorials) {
+      const totalSeconds = this.moduleData.tutorials.reduce((total, tutorial) => total + tutorial.durationSeconds, 0);
+      this.totalDurationFormatted = this.formatDuration(totalSeconds);
+    }
+  }
+
+  private formatDuration(seconds: number): string {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secondsLeft = seconds % 60;
+    return `${hours}h ${minutes}m ${secondsLeft}s`;
+  }
+
 }
---
+++ src/app/shared/icons/module-icon/module-icon.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input} from '@angular/core';
+import {NgClass, NgIf, NgSwitch, NgSwitchCase} from "@angular/common";
+
+@Component({
+  selector: 'ldnf-module-icon',
+  standalone: true,
+  imports: [
+    NgClass,
+    NgSwitch,
+    NgSwitchCase,
+    NgIf
+  ],
+  templateUrl: './module-icon.component.html',
+  styleUrl: './module-icon.component.css'
+})
+export class ModuleIconComponent {
+
+  @Input() color: "INDIGO" | "TEAL" | "PURPLE" | "PINK" | "YELLOW" | "FUCHSIA" | "ROSE" | "SKY" | undefined = "INDIGO";
+  @Input() icon : "BOOK" | "FLAG" | "TERMINAL" | "WARNING" | "SHIELD" | "DATABASE" | "LOCK" | "PLANE" | undefined;
+  @Input() size: "SMALL" | "LARGE" | undefined;
+
+  get colorStyles(): string {
+    const lowerCaseColor: string = this.color ? this.color.toLowerCase() : '';
+    const textColorLevel: string = (this.color === "FUCHSIA" || this.color === 'ROSE' || this.color === 'SKY') ? '600' : '700';
+
+    return `bg-${lowerCaseColor}-100/75 text-${lowerCaseColor}-${textColorLevel}`;
+  }
+  get sizeStyles(): string {
+    return this.size === 'LARGE' ? 'p-3' : 'p-1.5';
+  }
+
+  get colorAndSizeStyles(): string {
+    return this.colorStyles + ' ' + this.sizeStyles;
+  }
+}
