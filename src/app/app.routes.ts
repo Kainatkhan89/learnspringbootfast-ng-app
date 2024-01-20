3ba4255dff95d830f77df4fc751c4c4b8152edb9
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-20 13:58:43 -0400

    add forgot password route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,5 @@ export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./features/home-page/home-page.component').then(mod => mod.HomePageComponent) },
   { path: 'sign-up', loadComponent: () => import('./features/sign-up-page/sign-up-page.component').then(mod => mod.SignUpPageComponent) },
   { path: 'sign-in', loadComponent: () => import('./features/sign-in-page/sign-in-page.component').then(mod => mod.SignInPageComponent) },
+  { path: 'forgot-password', loadComponent: () => import('./features/forgot-password-page/forgot-password-page.component').then(mod => mod.ForgotPasswordPageComponent) },
 ];
