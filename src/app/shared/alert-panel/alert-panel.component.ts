891169ec534640e0c0ef14b11e5614ab3e3b7f04
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-19 20:07:03 -0400

    create alert panel component

---
+++ src/app/shared/alert-panel/alert-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ldnf-alert-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-panel.component.html',
+  styleUrl: './alert-panel.component.css'
+})
+export class AlertPanelComponent {
+
+}
