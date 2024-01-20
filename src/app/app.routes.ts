35ea1521270c9d8d57d6eda01ebd5a2c51b18ff6
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-20 14:21:02 -0400

    remove unused imports from app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import {LandingPageComponent} from "./features/landing-page/landing-page.component";
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./features/landing-page/landing-page.component').then(mod => mod.LandingPageComponent) },
