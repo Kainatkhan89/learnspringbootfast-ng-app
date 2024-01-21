aa8914ee5cc4f10d540c7c319d0e9b85ab1d8b14
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-20 22:09:07 -0400

    setup wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,5 @@ export const routes: Routes = [
   { path: 'sign-up', loadComponent: () => import('./features/sign-up-page/sign-up-page.component').then(mod => mod.SignUpPageComponent) },
   { path: 'sign-in', loadComponent: () => import('./features/sign-in-page/sign-in-page.component').then(mod => mod.SignInPageComponent) },
   { path: 'forgot-password', loadComponent: () => import('./features/forgot-password-page/forgot-password-page.component').then(mod => mod.ForgotPasswordPageComponent) },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
