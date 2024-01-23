5816579eb8782a4398bfccc0e08b17b452073b9a
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-23 14:55:16 -0400

    generate tutorial page

---
+++ src/app/features/tutorial-page/tutorial-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsbf-tutorial-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-page.component.html',
+  styleUrl: './tutorial-page.component.css'
+})
+export class TutorialPageComponent {
+
+}
