f8139b793c06b94db3eb2507658f137ac56875a5
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-22 20:35:43 -0400

    combine learning path and progress data

--- src/app/core/services/user-learning-data/user-learning-data.service.ts
+++ src/app/core/services/user-learning-data/user-learning-data.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {LearningPathService} from "../learning-path/learning-path.service";
+import {ProgressDataService} from "../progress/progress-data.service";
+import {combineLatest, map, Observable} from "rxjs";
+import {ILearningPath} from "../../models/learning-path/learning-path.model";
+import {IProgress} from "../../models/progress/progress.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserLearningDataService {
+  private _learningPathService: LearningPathService = inject(LearningPathService);
+  private _progressDataService: ProgressDataService = inject(ProgressDataService);
 
-  constructor() { }
+  private _learningPath$: Observable<ILearningPath>;
+  private _progressData$: Observable<IProgress>;
+
+  userLearningData$: Observable<ILearningPath> | undefined;
+
+  constructor() {
+    this._learningPath$ = this._learningPathService.getLearningPath();
+    this._progressData$ = this._progressDataService.getUserProgress();
+
+    this._initializeData();
+  }
+
+  private _initializeData(): void {
+    this.userLearningData$ = combineLatest([this._learningPath$, this._progressData$]).pipe(
+      map(([learningPath , progressData]) => {
+        let totalTutorials: number = 0;
+        let completedCount: number = 0;
+
+        learningPath.modules = learningPath.modules.map(module => {
+          module.tutorials = module.tutorials.map(tutorial => {
+            totalTutorials++;
+
+            const completed = progressData.completedTutorialIds.some(completedTutorialId => completedTutorialId === tutorial.id);
+            if (completed) {
+              completedCount++;
+            }
+
+            return { ...tutorial, completed };
+          })
+
+          return module;
+        });
+
+        return learningPath;
+      })
+    );
+  }
 }
