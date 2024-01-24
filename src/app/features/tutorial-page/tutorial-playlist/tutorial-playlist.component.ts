2d31d3a282cd030ff3179fe5debaf7777207681b
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-24 18:53:46 -0400

    define animations

--- src/app/features/tutorial-page/tutorial-playlist/tutorial-playlist.component.ts
+++ src/app/features/tutorial-page/tutorial-playlist/tutorial-playlist.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from '@angular/core';
 import {ILearningPath} from "../../../core/models/learning-path/learning-path.model";
 import {RouterLink} from "@angular/router";
 import {NgForOf, NgIf} from "@angular/common";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'lsbf-tutorial-playlist',
@@ -9,10 +10,33 @@ import {NgForOf, NgIf} from "@angular/common";
   imports: [
     RouterLink,
     NgForOf,
-    NgIf
+    NgIf,
+
   ],
   templateUrl: './tutorial-playlist.component.html',
-  styleUrl: './tutorial-playlist.component.css'
+  styleUrl: './tutorial-playlist.component.css',
+  animations: [
+    trigger('backdropAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in-out', style({  opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('500ms ease-in-out', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('slideOverAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms ease-in-out', style({  transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)' }),
+        animate('500ms ease-in-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class TutorialPlaylistComponent {
   @Input() userLearningData: ILearningPath | undefined;
