bf3d95c31b5db09f4a8a90069a2dc113f9dad865
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-24 18:55:51 -0400

    provide animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,14 @@ import {InMemoryWebApiModule} from "angular-in-memory-web-api";
 import {LearningPathData} from "./core/services/learning-path/learning-path.data";
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
     importProvidersFrom(InMemoryWebApiModule.forRoot(LearningPathData, { delay: 1000 })),
     importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"learnspringbootfast","appId":"1:369723666075:web:3e82892c2077fbfa2f203e","storageBucket":"learnspringbootfast.appspot.com","apiKey":"AIzaSyBRFSwhMaj1tqi9W7zv5z8_uroqkSdp95A","authDomain":"learnspringbootfast.firebaseapp.com","messagingSenderId":"369723666075"}))),
     importProvidersFrom(provideAuth(() => getAuth())),]
