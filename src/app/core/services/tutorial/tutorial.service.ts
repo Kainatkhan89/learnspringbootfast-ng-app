682d6c027a5491a540607348e39cdf1da36061e3
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-23 20:17:15 -0400

    setup tutorial service with current and all tutorials

--- src/app/core/services/tutorial/tutorial.service.ts
+++ src/app/core/services/tutorial/tutorial.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {LearningPathService} from "../learning-path/learning-path.service";
+import {map, Observable, Subject} from "rxjs";
+import {ITutorial} from "../../models/learning-path/tutorial.model";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
+  private _learningPathService: LearningPathService = inject(LearningPathService);
+
+  currentTutorialSubject: Subject<ITutorial> = new Subject<ITutorial>();
+  currentTutorial$: Observable<ITutorial> | undefined;
 
   constructor() { }
+
+  setCurrentTutorial(tutorial: ITutorial): void {
+    this.currentTutorialSubject.next(tutorial);
+  }
+
+  getAllLearningPathTutorials(): Observable<ITutorial[]> {
+    return this._learningPathService.getLearningPath().pipe(
+      map((learningPath => {
+        return learningPath.modules.flatMap(module => module.tutorials);
+      }))
+    );
+  }
 }
--- src/app/features/tutorial-page/tutorial-page.component.ts
+++ src/app/features/tutorial-page/tutorial-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'lsbf-tutorial-page',
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './tutorial-page.component.html',
   styleUrl: './tutorial-page.component.css'
 })
-export class TutorialPageComponent {
+export class TutorialPageComponent implements OnInit, OnDestroy {
 
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
 }
