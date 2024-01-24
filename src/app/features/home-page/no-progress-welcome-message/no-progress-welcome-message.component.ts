782f3608563c00bb3bf9aad7498c5848baeb797c
kainatkhan89 <kainatkhan892014@gmail.com>
2024-01-23 20:29:08 -0400

    set first tutorial as current one before navigating to tutorial

--- src/app/features/home-page/no-progress-welcome-message/no-progress-welcome-message.component.ts
+++ src/app/features/home-page/no-progress-welcome-message/no-progress-welcome-message.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component, inject, Input} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 import {WavingHandComponent} from "../../../shared/icons/waving-hand/waving-hand.component";
 import {ITutorial} from "../../../core/models/learning-path/tutorial.model";
 import {NgIf} from "@angular/common";
+import {TutorialService} from "../../../core/services/tutorial/tutorial.service";
 
 @Component({
   selector: 'lsbf-no-progress-welcome-message',
@@ -17,4 +18,14 @@ import {NgIf} from "@angular/common";
 })
 export class NoProgressWelcomeMessageComponent {
   @Input() firstTutorial: ITutorial | undefined;
+
+  private _tutorialService: TutorialService = inject(TutorialService);
+  private _router: Router = inject(Router);
+
+  navigateToFirstTutorial(): void {
+    if (this.firstTutorial) {
+      this._tutorialService.setCurrentTutorial(this.firstTutorial);
+      this._router.navigate(['/tutorial']);
+    }
+  }
 }
