5dcc225450655c69b384deae44f92a569263d691
kainatkhan89 <kainatkhan892014@gmail.com>
2024-02-01 16:28:47 -0400

    determine if currently playing

--- src/app/features/tutorial-page/tutorial-playlist/playlist-module-contents/tutorial-playlist-item/tutorial-playlist-item.component.ts
+++ src/app/features/tutorial-page/tutorial-playlist/playlist-module-contents/tutorial-playlist-item/tutorial-playlist-item.component.ts
@@ -1,18 +1,24 @@
 import {Component, Input} from '@angular/core';
 import {ITutorial} from "../../../../../core/models/learning-path/tutorial.model";
 import {RouterLink} from "@angular/router";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'lsbf-tutorial-playlist-item',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf,
+    NgClass
   ],
   templateUrl: './tutorial-playlist-item.component.html',
   styleUrl: './tutorial-playlist-item.component.css'
 })
 export class TutorialPlaylistItemComponent {
   @Input() tutorial: ITutorial | undefined;
+  @Input() currentlyPlayingTutorialId: number | undefined;
 
-
+  get isCurrentlyPlaying(): boolean {
+    return this.tutorial && this.currentlyPlayingTutorialId ? this.currentlyPlayingTutorialId === this.tutorial.id : false;
+  }
 }
