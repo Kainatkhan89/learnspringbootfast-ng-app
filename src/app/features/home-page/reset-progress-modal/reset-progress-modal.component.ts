c38e9ef728fc171efba82c408cde00c9eef6f6ca
kainatkhan89 <kainatkhan892014@gmail.com>
2024-02-01 13:20:01 -0400

    generate reset progress modal component

---
+++ src/app/features/home-page/reset-progress-modal/reset-progress-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lsbf-reset-progress-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-progress-modal.component.html',
+  styleUrl: './reset-progress-modal.component.css'
+})
+export class ResetProgressModalComponent {
+
+}
