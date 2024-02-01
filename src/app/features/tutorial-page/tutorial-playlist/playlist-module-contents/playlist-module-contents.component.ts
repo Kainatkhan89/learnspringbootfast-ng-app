1b645783c8377b7fc7a41f93067a7f663ac24a9a
kainatkhan89 <kainatkhan892014@gmail.com>
2024-02-01 15:17:47 -0400

    style module contents component

--- src/app/features/tutorial-page/tutorial-playlist/playlist-module-contents/playlist-module-contents.component.ts
+++ src/app/features/tutorial-page/tutorial-playlist/playlist-module-contents/playlist-module-contents.component.ts
@@ -1,12 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {IModule} from "../../../../core/models/learning-path/module.model";
 import {NgIf} from "@angular/common";
+import {ModuleIconComponent} from "../../../../shared/icons/module-icon/module-icon.component";
 
 @Component({
   selector: 'lsbf-playlist-module-contents',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    ModuleIconComponent
   ],
   templateUrl: './playlist-module-contents.component.html',
   styleUrl: './playlist-module-contents.component.css'
